9b51a2b052b0e48fce8e36e459ded53a6996f356
MicaCarballo <micacarballo2@gmail.com>
2023-11-21 19:51:16 -0800

    first commit

---
+++ mail-contact.php
@@ -0,0 +1,16 @@
+<?php
+
+	$to = "micacarballo2@gmail.com"; // this is your Email address
+	$from  = $_POST['email']; // this is the sender's Email address
+	$sender_name = $_POST['name'];
+	
+	$note = $_POST['note'];
+
+	$subject = "Form submission";
+
+	$message = $sender_name . " has send the contact message. His / Her contact subject is " .  $subject . " and his / her phone number is " . $phone . ". He / she wrote the following... ". "\n\n" . $note;
+
+	$headers = 'From: ' . $from;
+	mail($to, $subject, $message, $headers);
+
+?>
